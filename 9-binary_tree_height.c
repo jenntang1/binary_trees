e945f48fb8f5623035c908866ffa9084b8f2ec36
justinmajetich <justinmajetich@gmail.com>
2020-03-10 15:08:51 +0000

    Change counting mechanism to ignore root

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+size_t recurse_for_height(const binary_tree_t *tree);
 /**
  * binary_tree_height - measure height of tree
  * @tree: tree to measure
@@ -7,18 +8,26 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t heightR = 0, heightL = 0;
+	return (recurse_for_height(tree) - 1);
+}
+/**
+ * recurse_for_height - measure height
+ * @tree: tree to measure
+ *
+ * Return: height
+ */
+size_t recurse_for_height(const binary_tree_t *tree)
+{
+	size_t heightR, heightL;
 
-	if (tree)
-	{
-		/* recurse */
-		heightL = binary_tree_height(tree->left);
-		heightR = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
 
-		if (heightL > heightR)
-			return (heightL);
-		else
-			return (heightR);
-	}
-	return (0);
+	heightL = recurse_for_height(tree->left);
+	heightR = recurse_for_height(tree->right);
+
+	if (heightL > heightR)
+		return (heightL + 1);
+	else
+		return (heightR + 1);
 }
