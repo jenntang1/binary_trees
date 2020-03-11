94c72344aacddbc170cd005424b06397ea0162fe
justinmajetich <justinmajetich@gmail.com>
2020-03-11 18:01:33 +0000

    Implement balance check to ensure all trees/subtrees are perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
-
+int binary_tree_balance(const binary_tree_t *tree);
 size_t recurse_for_height(const binary_tree_t *tree);
-
+int recurse_for_balance(const binary_tree_t *tree);
 /**
  * binary_tree_is_perfect - checks for perfect binary tree
  * @tree: pointer to the root node
@@ -11,6 +11,46 @@ size_t recurse_for_height(const binary_tree_t *tree);
  * 2. upon fail, return 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	/* base case */
+	if (tree == NULL)
+		return (0);
+
+	/* check if all trees/subtrees have balance factor of 0 */
+	if (recurse_for_balance(tree) == 0)
+			return (1);
+	return (0);
+}
+/**
+ * recurse_for_balance - utility for checking tree and subtrees
+ * @tree: pointer to root of tree
+ *
+ * Return: balance factor
+ */
+int recurse_for_balance(const binary_tree_t *tree)
+{
+    int balFactor;
+
+    if (!tree)
+        return (0);
+
+    /* take balance factor of every tree/subtree */
+    balFactor = binary_tree_balance(tree);
+
+    if (balFactor != 0)
+        return (balFactor);
+
+    return (recurse_for_balance(tree->left) || recurse_for_balance(tree->right));
+}
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to node to measure the balance factor
+ * Description: 14. Balance factor
+ * Return: see below
+ * 1. upon success, return balance factor
+ * 2. upon fail, return 0
+ */
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	/* declare and initialize variables to calculate the heights */
 	int left = 0;
@@ -22,39 +62,19 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	/* if root node has no children */
+	/* if given node has no balance factor */
 	if ((tree->left == NULL) && (tree->right == NULL))
 	{
-		return (1);
+		return (0);
 	}
 
 	/* calculate height of left and right subtrees */
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left = recurse_for_height(tree->left) - 1;
+	right = recurse_for_height(tree->right) - 1;
 
-	/* if balance is 0, then it's perfect */
-	if ((left - right) == 0)
-	{
-		return (1);
-	}
-
-	return (0);
+	/* balance factor = left height - right height */
+	return (left - right);
 }
-
-/**
- * binary_tree_height - measure height of tree
- * @tree: tree to measure
- *
- * Return: height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	return (recurse_for_height(tree) - 1);
-}
-
 /**
  * recurse_for_height - measure height
  * @tree: tree to measure
