28db70df90943c8251f8c183345c66216b2f88b4
jenntang1 <1039@holbertonschool.com>
2020-03-12 03:18:45 +0000

    Update comments

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-bst_t *minimum(bst_t *root);
+bst_t *helper_loop(bst_t *root);
 
 /**
  * bst_remove - removes a node from a BST
@@ -13,12 +13,12 @@ bst_t *minimum(bst_t *root);
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	/* declare and initialize pointers */
-	bst_t *new = NULL, *current = NULL;
+	/* declare and initialize pointer */
+	bst_t *new = NULL;
 
-	/* if there's no root node */
+	/* if there's no root node or no given value */
 	if (root == NULL)
-		return (NULL);
+		return (root);
 
 	/* first search for value on the left subtree */
 	/* then search for value on the right subtree */
@@ -26,16 +26,23 @@ bst_t *bst_remove(bst_t *root, int value)
 		root->left = bst_remove(root->left, value);
 	else if (root->n < value)
 		root->right = bst_remove(root->right, value);
-	else
+
+	if (root->n == value)
 	{
-		/* if there's no child or one right child */
+		/* if there's no children */
+		if ((root->left == NULL) && (root->right == NULL))
+		{
+			free(root);
+			return (NULL);
+		}
+		/* if there's one right child */
 		if (root->left == NULL)
 		{
 			new = root->right;
 			free(root);
 			return (new);
 		}
-		/* if there's no child or one left child */
+		/* if there's one left child */
 		else if (root->right == NULL)
 		{
 			new = root->left;
@@ -43,26 +50,27 @@ bst_t *bst_remove(bst_t *root, int value)
 			return (new);
 		}
 		/* if there's two children */
-		current = minimum(root->right);
-		root->n = current->n;
-		root->right = bst_remove(root->right, current->n);
+		else
+		{
+			new = helper_loop(root->right);
+			root->n = new->n;
+			root->right = bst_remove(root->right, new->n);
+		}
 	}
 	return (root);
 }
 
 /**
- * minimum - searches for minimum value in BST
+ * helper_loop - searches for in-order successor
  * @root: pointer to root node
- * Description: search in left subtree only
- * Return: minimum value
+ * Description: searches in left subtree only
+ * Return: pointer to in-order successor
  */
-bst_t *minimum(bst_t *root)
+bst_t *helper_loop(bst_t *root)
 {
 	bst_t *current = root;
 
 	while ((current) && (current->left))
-	{
 		current = current->left;
-	}
 	return (current);
 }
