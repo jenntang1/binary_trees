afabca91ddb7df9d653c31d95880d150e4dbfeec
justinmajetich <justinmajetich@gmail.com>
2020-03-13 21:07:20 +0000

    Remove blank lines from header

--- binary_trees.h
+++ binary_trees.h
@@ -70,8 +70,6 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-
-
 /* UTILITIES */
 void binary_tree_print(const binary_tree_t *);
 
